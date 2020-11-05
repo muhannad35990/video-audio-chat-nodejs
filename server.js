beeb2735ac99fd39f94b4edb1238403c848593b8
unknown <muhannad35990@gmail.com>
2020-11-05 12:43:44 +0200

    fixes2722

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const peerServer = PeerServer({
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
