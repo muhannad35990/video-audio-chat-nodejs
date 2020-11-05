8ca9b9c595469d88cfc2048c2e7e67f8f4d271e4
unknown <muhannad35990@gmail.com>
2020-11-05 12:14:33 +0200

    fixes

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidV4 } = require("uuid");
 
-//middlewares
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-//get and post
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+  res.redirect(`/${uuidV4()}`);
 });
 
 app.get("/:room", (req, res) => {
